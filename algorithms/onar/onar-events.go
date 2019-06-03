7a731069aabdd474e18ab2fe486404587ef8a6f7
Alexandru Duma <badco25@gmail.com>
2019-06-03 19:11:55 +0300

    ONAR algorithm

---
+++ algorithms/onar/onar-events.go
@@ -0,0 +1,99 @@
+package onar
+
+import (
+	"log"
+	"os"
+
+	"github.com/alex-d-tc/distributed-systems-algorithms/util"
+)
+
+type ReadReturn struct {
+	value int64
+}
+
+type onReadReturnManager struct {
+	listeners []chan<- ReadReturn
+
+	internalHandler util.EventHandler
+
+	logger *log.Logger
+}
+
+func NewOnReadReturnManager() *onReadReturnManager {
+
+	manager := &onReadReturnManager{
+		listeners: make([]chan<- ReadReturn, 0),
+		logger:    log.New(os.Stdout, "[ONAROnReadReturnManager]", log.Ldate|log.Ltime),
+	}
+
+	manager.internalHandler = util.NewEventHandler(manager.handleMessage)
+
+	return manager
+}
+
+func (manager *onReadReturnManager) handleMessage(ev interface{}) {
+	switch ev.(type) {
+	case chan ReadReturn:
+		manager.listeners = append(manager.listeners, ev.(chan<- ReadReturn))
+		break
+	case ReadReturn:
+		for _, listener := range manager.listeners {
+			listener <- ev.(ReadReturn)
+		}
+	}
+}
+
+func (manager *onReadReturnManager) AddListener() <-chan ReadReturn {
+	listener := make(chan ReadReturn, 1)
+	manager.internalHandler.Submit(listener)
+	return listener
+}
+
+func (manager *onReadReturnManager) Submit(ev ReadReturn) {
+	manager.internalHandler.Submit(ev)
+}
+
+type WriteReturn struct {
+}
+
+type onWriteReturnManager struct {
+	listeners []chan<- WriteReturn
+
+	internalHandler util.EventHandler
+
+	logger *log.Logger
+}
+
+func NewOnWriteReturnManager() *onWriteReturnManager {
+
+	manager := &onWriteReturnManager{
+		listeners: make([]chan<- WriteReturn, 0),
+		logger:    log.New(os.Stdout, "[ONAROnWriteReturnManager]", log.Ldate|log.Ltime),
+	}
+
+	manager.internalHandler = util.NewEventHandler(manager.handleMessage)
+
+	return manager
+}
+
+func (manager *onWriteReturnManager) handleMessage(ev interface{}) {
+	switch ev.(type) {
+	case chan WriteReturn:
+		manager.listeners = append(manager.listeners, ev.(chan<- WriteReturn))
+		break
+	case WriteReturn:
+		for _, listener := range manager.listeners {
+			listener <- ev.(WriteReturn)
+		}
+	}
+}
+
+func (manager *onWriteReturnManager) AddListener() <-chan WriteReturn {
+	listener := make(chan WriteReturn, 1)
+	manager.internalHandler.Submit(listener)
+	return listener
+}
+
+func (manager *onWriteReturnManager) Submit(ev WriteReturn) {
+	manager.internalHandler.Submit(ev)
+}
---
+++ algorithms/onar/onar.go
@@ -0,0 +1,213 @@
+package onar
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"sync"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/alex-d-tc/distributed-systems-algorithms/algorithms/beb"
+	"github.com/alex-d-tc/distributed-systems-algorithms/algorithms/pfd"
+	"github.com/alex-d-tc/distributed-systems-algorithms/protocol"
+	"github.com/alex-d-tc/distributed-systems-algorithms/util"
+	"github.com/alex-d-tc/distributed-systems-algorithms/util/environment"
+)
+
+var ackBytes = [...]byte{0, 8, 3}
+
+type ONAR struct {
+
+	// Algorithm-relevant data
+	timestamp uint64
+	val       int64
+
+	correctProcesses []string
+	writeset         map[string]bool
+
+	readval int64
+	reading bool
+
+	// Algorithm dependencies
+	beb      *beb.BestEffortBroadcast
+	pfd      *pfd.PerfectFailureDetector
+	onarPort uint16
+
+	// Exposed events
+	onReadReturn  *onReadReturnManager
+	onWriteReturn *onWriteReturnManager
+
+	// External event queues
+	pfdOnCrashedListener <-chan string
+	bebOnDeliverListener <-chan beb.BestEffortBroadcastMessage
+
+	// Logging
+	logger *log.Logger
+
+	// Extras
+	stateLock *sync.Mutex
+}
+
+func NewONAR(env *environment.NetworkEnvironment, beb *beb.BestEffortBroadcast, pfd *pfd.PerfectFailureDetector) *ONAR {
+
+	onar := &ONAR{
+		onarPort:         env.GetONARPort(),
+		beb:              beb,
+		pfd:              pfd,
+		timestamp:        0,
+		val:              0,
+		correctProcesses: env.GetHosts(),
+		writeset:         make(map[string]bool, 0),
+		readval:          0,
+		reading:          false,
+		onReadReturn:     NewOnReadReturnManager(),
+		onWriteReturn:    NewOnWriteReturnManager(),
+		logger:           log.New(os.Stdout, "[ONAR]", log.Ldate|log.Ltime),
+		stateLock:        &sync.Mutex{},
+	}
+
+	onar.pfdOnCrashedListener = pfd.AddOnProcessCrashedListener()
+	onar.bebOnDeliverListener = beb.AddOnDeliverListener()
+
+	go onar.handleBebDeliver()
+	go onar.handleProcessCrash()
+	go util.Listen(onar.onarPort, onar.handleAckMessage, onar.logger)
+
+	return onar
+}
+
+func (onar *ONAR) Read() error {
+	onar.reading = true
+	onar.readval = onar.val
+
+	writeMessage := &protocol.ONARWriteMessage{
+		Timestamp: onar.timestamp,
+		Val:       onar.val,
+	}
+
+	// Send write message
+	rawMsg, err := proto.Marshal(writeMessage)
+	if err != nil {
+		return err
+	}
+
+	onar.beb.Broadcast(rawMsg)
+	return nil
+}
+
+func (onar *ONAR) Write(val int64) error {
+
+	writeMessage := &protocol.ONARWriteMessage{
+		Timestamp: onar.timestamp + 1,
+		Val:       val,
+	}
+
+	// Send write message
+	rawMsg, err := proto.Marshal(writeMessage)
+	if err != nil {
+		return err
+	}
+
+	onar.beb.Broadcast(rawMsg)
+	return nil
+}
+
+func (onar *ONAR) handleAckMessage(conn net.Conn) {
+
+	defer conn.Close()
+
+	ack, err := ioutil.ReadAll(conn)
+	if err != nil {
+		return
+	}
+
+	// Check whether the ackbytes are OK
+	if !(bytes.Equal(ack, ackBytes[:])) {
+		return
+	}
+
+	onar.stateLock.Lock()
+	defer onar.stateLock.Unlock()
+
+	// Add the caller to the writeset
+	onar.writeset[conn.RemoteAddr().(*net.TCPAddr).IP.String()] = true
+
+	onar.tryDelivering()
+}
+
+func (onar *ONAR) handleProcessCrash() {
+
+	for {
+		crashedHost := <-onar.pfdOnCrashedListener
+
+		onar.stateLock.Lock()
+
+		// Remove the crashed process from the crashed processes list
+		for i := 0; i < len(onar.correctProcesses); i++ {
+			if onar.correctProcesses[i] == crashedHost {
+				onar.correctProcesses[i] = onar.correctProcesses[len(onar.correctProcesses)-1]
+				onar.correctProcesses = onar.correctProcesses[:len(onar.correctProcesses)-1]
+				break
+			}
+		}
+
+		onar.tryDelivering()
+		onar.stateLock.Unlock()
+	}
+
+}
+
+func (onar *ONAR) handleBebDeliver() {
+
+	for {
+		bebMsg := <-onar.bebOnDeliverListener
+
+		// Get underlying ONARWriteMessage
+		onarMsg := &protocol.ONARWriteMessage{}
+		err := proto.Unmarshal(bebMsg.Message, onarMsg)
+		if err != nil {
+			continue
+		}
+
+		if onarMsg.GetTimestamp() > onar.timestamp {
+			onar.timestamp = onarMsg.GetTimestamp()
+			onar.val = onarMsg.GetVal()
+		}
+
+		// Send ack message to the ONAR process
+		err = util.SendMessage(bebMsg.SourceHost, onar.onarPort, ackBytes[:])
+		if err != nil {
+			continue
+		}
+	}
+
+}
+
+func (onar *ONAR) tryDelivering() {
+
+	// Check whether all correct processes are part of the writeset
+	// If it is the case, end the current write or read operation
+	if onar.allCorrectHaveAcked() {
+		onar.writeset = make(map[string]bool, 0)
+		if onar.reading {
+			onar.reading = false
+			onar.onReadReturn.Submit(ReadReturn{value: onar.readval})
+		} else {
+			onar.onWriteReturn.Submit(WriteReturn{})
+		}
+	}
+}
+
+func (onar *ONAR) allCorrectHaveAcked() bool {
+
+	for _, correctHost := range onar.correctProcesses {
+		if !onar.writeset[correctHost] {
+			return false
+		}
+	}
+
+	return true
+}
